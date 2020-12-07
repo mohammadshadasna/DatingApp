82b9daf065f8d061b4c1547bfbb017cd93b15d5b
mshadamana <mohammadshadeducation@gmail.com>
2020-12-07 12:08:23 +0300

    update to section 4

--- DatingApp/src/app/app.module.ts
+++ DatingApp/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
+  
   ],
   imports: [
     BrowserModule,
